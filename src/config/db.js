eddc47209b64a453e30aec7232b233c9408d5611
Harsh Kumar <126059255+teamharsh@users.noreply.github.com>
2024-10-03 22:45:52 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
   } catch (err) {
